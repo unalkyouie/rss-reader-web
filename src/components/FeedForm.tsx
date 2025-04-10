9a7ef133afa0a2395dd2abc49f87d46cc6d5e289
aleksandra.pikus@solidstudio.io <aleksandra.pikus@solidstudio.io>
2025-04-10 20:43:42 +0200

    feat: FeedForm component to add new feed

---
+++ src/components/FeedForm.tsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+
+type Props = {
+    onAddFeed: (feed: {name: string, url: string}) => void;
+}
+
+const FeedForm = ({onAddFeed}:Props) => {
+    const [name, setName]=useState('');
+    const [url, setUrl] = useState('');
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        if( name && url){
+            onAddFeed({name, url});
+            setName('');
+            setUrl('');
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                placeholder='Feed Name'
+                value={name}
+                onChange={(e)=>e.target.value}
+                required 
+                />
+            <input 
+                type='url'
+                placeholder='Feed URL'
+                value={url}
+                onChange={(e)=>setUrl(e.target.value)}
+                required
+                />
+                <button type='submit'>Add feed</button>
+        </form>
+
+    )
+};
+
+export default FeedForm;
