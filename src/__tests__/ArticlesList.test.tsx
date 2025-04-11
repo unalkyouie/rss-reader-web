bb041d3b89cd50b898867066e77df44780d04873
aleksandra.pikus@solidstudio.io <alk.pikus@gmail.com>
2025-04-11 11:40:37 +0200

    feat: create ArticlesList component

--- src/__tests__/ArticlesList.test.tsx
+++ src/__tests__/ArticlesList.test.tsx
@@ -1,16 +1,18 @@
 import { render } from "@testing-library/react"
 import { mockArticles } from "./__mocks__"
 import { screen } from "@testing-library/dom";
+import ArticlesList from "../components/ArticlesList";
+
 
 describe('ArticlesList', ()=>{
     it('renders a list of articles sorted by date descending', ()=>{
-        render(<ArticlesList acrticles={mockArticles}/>);
+        render(<ArticlesList articles={mockArticles}/>);
 
         const titles = screen.getAllByRole('link').map((el)=>el.textContent);
         expect(titles).toEqual(['Second Article', 'First Article']);
     });
     it('displays publication date and feed title for each article', ()=>{
-        render(<ArticlesList acrticles={mockArticles}/>);
+        render(<ArticlesList articles={mockArticles}/>);
 
         expect(screen.getByText(/Example Title 1/)).toBeInTheDocument();
         expect(screen.getByText(/Example Title 2/)).toBeInTheDocument();
