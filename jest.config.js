8751263ac48869eb006556a73a687d8e22a90340
aleksandra.pikus@solidstudio.io <alk.pikus@gmail.com>
2025-04-15 15:52:54 +0200

    Update Jest config to ignore mocks directory

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-  testPathIgnorePatterns: ['/node_modules/', '/__mocks__/'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/__tests__/__mocks__/'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
