c263d32f30d6f6c02625afa79590ccf451ad7fda
aleksandra.pikus@solidstudio.io <aleksandra.pikus@solidstudio.io>
2025-04-15 15:46:48 +0200

    chore: fix configuration files and update husky hooks for v10 compatibility

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/__mocks__/'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
