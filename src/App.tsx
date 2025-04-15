130c3a9426363b52df658f5ff9cedc929e8cedfc
aleksandra.pikus@solidstudio.io <aleksandra.pikus@solidstudio.io>
2025-04-15 15:30:57 +0200

    feat: update App component and use proxy for RSS feeds

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import FeedList from './components/FeedList';
-import { mockArticles, mockFeeds } from './__tests__/__mocks__';
+import { mockFeeds } from './__tests__/__mocks__';
 import { Feed } from './types';
 import FeedForm from './components/FeedForm';
 import ArticlesList from './components/ArticlesList';
 
 
 const App: React.FC = ()=> {
+  const [feeds, setFeeds] = useState<Array<Feed>>([]);
 
-const [feeds, setFeeds] = useState<Array<Feed>>([]);
-
-useEffect(()=>{
-  setFeeds(mockFeeds)
-},[])
+  useEffect(()=>{
+    console.log('App component mounted');
+    setFeeds(mockFeeds);
+  },[]);
 
   return (
    <div>
     <h1>RSS Reader</h1>
     <FeedList feeds={feeds}/>
-    <ArticlesList articles={mockArticles} />
+    <ArticlesList url={'/api/rss'} />
     <FeedForm onAddFeed={(feed)=>{console.log(feed)}} />
    </div>
   );
