c01cd0938cd13703900506d05464e0efeb068daa
Aleksandra Pikus <alk.pikus@gmail.com>
2025-04-20 21:05:45 +0200

    refactor: mark articles as read

--- src/features/articles/ArticlesGrid.tsx
+++ src/features/articles/ArticlesGrid.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Article } from '~/types/global';
 
 interface Props {
@@ -7,20 +7,30 @@ interface Props {
 }
 
 const ArticlesGrid = ({ articles }: Props) => {
-  localStorage.setItem('articles', JSON.stringify(articles));
+  const navigate = useNavigate();
+  const readArticles = useRef(
+    new Set<string>(JSON.parse(localStorage.getItem('readArticles') || '[]')),
+  );
+
+  const handleSelectFeed = useCallback((id: string) => {
+    readArticles.current.add(id);
+    localStorage.setItem('readArticles', JSON.stringify([...readArticles.current]));
+    navigate(`/articles/${id}`);
+  }, []);
 
   return (
     <div className="articles-grid" data-testid="articles-grid">
       {articles.map((article, index) => {
+        const isRead = readArticles.current.has(article.id ?? '');
         return (
           <div
             key={article.id || index}
-            className="article-item"
+            className={`article-item ${isRead ? 'read' : ''}`}
             data-testid={`article-item-${article.id}`}
           >
             {article.title && <h2>{article.title}</h2>}
             {article.description && <p>{article.description}</p>}
-            <Link to={`/articles/${article.id}`}>Read more</Link>
+            <button onClick={() => handleSelectFeed(article.id)}>Read more</button>
           </div>
         );
       })}
